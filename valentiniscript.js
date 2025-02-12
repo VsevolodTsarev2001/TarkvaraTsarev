97ab04a53acd805eda7f39d42e6a3b5dc24fec84
David-Lennuk <david.nikolajev07@gmail.com>
2025-02-12 13:19:29 +0200

    töötab JS-iga

--- valentiniscript.js
+++ valentiniscript.js
@@ -1,5 +1,16 @@
 document.getElementById('loveButton').addEventListener('click', function() {
     const heart = document.getElementById('heart');
+    const message = document.createElement('p');
+    message.textContent = 'Ты подарил сердечко!';
+    document.body.appendChild(message);
+
+    setTimeout(() => {
+        message.style.display = 'none';
+    }, 3000);
+
+    const audio = document.getElementById('audio');
+    audio.play();
+
     heart.style.transform = 'scale(1)';
     setTimeout(() => {
         heart.style.transform = 'scale(0)';
