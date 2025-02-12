414aaa76496bff48e1c8778947469b9295d370d9
David-Lennuk <david.nikolajev07@gmail.com>
2025-02-12 13:22:43 +0200

    töötada südamega

--- valentiniscript.js
+++ valentiniscript.js
@@ -16,3 +16,13 @@ document.getElementById('loveButton').addEventListener('click', function() {
         heart.style.transform = 'scale(0)';
     }, 1000);
 });
+document.getElementById('heart').addEventListener('click', function() {
+    //Получаем элемент с сердечком
+    const heart = document.getElementById('heart');
+
+    //случайный цвет в формате hex
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    //Меняем фона сердца на случайный
+    heart.style.backgroundColor = randomColor;
+});
